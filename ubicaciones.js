75660dcb96c3c178b10e56a649d0a90b5717cc5d
Matzuky <kacktusz_1@hotmail.com>
2018-02-23 13:59:47 -0600

    ubicaciones

---
+++ ubicaciones.js
@@ -0,0 +1,112 @@
+{
+  "errorCode": 0,
+  "errorString": "ok",
+  "hotspots": [
+    {"actions": [{
+          "contentType": "text/html",
+          "label": "Museo Textil",
+          "uri": "https:"
+        }
+      ],
+      "anchor": {
+        "geolocation": {
+          "lat": ,
+          "lon": 
+        }
+      },
+      "id": 1,
+      "text": {
+        "title": "Museo Textil de Oaxaca"
+      }
+    },
+    {"actions": [{
+          "contentType": "text/html",
+          "label": "Museo Infantil",
+          "uri": ""
+        }
+      ],
+      "anchor": {
+        "geolocation": {
+          "lat": ,
+          "lon": 
+        }
+      },
+      "id": 2,
+      "text": {
+        "title": "Museo Infantil"
+      }
+    },
+    {"actions": [{
+          "contentType": "text/html",
+          "label": "Foto 3 Iglesia Sangre de Cristo",
+          "uri": "https:"
+        }
+      ],
+      "anchor": {
+        "geolocation": {
+          "lat": ,
+          "lon": 
+        }
+      },
+      "id": 3,
+      "text": {
+        "title": "Iglesia Sangre de Cristo"
+      }
+    },
+    {"actions": [{
+          "contentType": "text/html",
+          "label": "El Llano",
+          "uri": "https:"
+        }
+      ],
+      "anchor": {
+        "geolocation": {
+          "lat": ,
+          "lon": 
+        }
+      },
+      "id": 4,
+      "text": {
+        "title": "El Llano"
+      }
+    },
+    {"actions": [{
+          "contentType": "text/html",
+          "label": "Teatro Macedonio Alcala",
+          "uri": "https:"
+        }
+      ],
+      "anchor": {
+        "geolocation": {
+          "lat": ,
+          "lon": 
+        }
+      },
+      "id": 5,
+      "text": {
+        "title": "Teatro Macedonio Alcala"
+      }
+    },
+    {"actions": [{
+          "contentType": "text/html",
+          "label": "IAGO",
+          "uri": ""
+        }
+      ],
+      "anchor": {
+        "geolocation": {
+          "lat": ,
+          "lon": 
+        }
+      },
+      "id": 6,
+      "text": {
+        "title": "IAGO"
+      }
+    }
+  ],
+  "layer": "layer189v5",
+  "morePages": false,
+  "nextPageKey": null,
+  "referenceImages": []
+}
